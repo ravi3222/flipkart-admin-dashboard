376fa1d7e25a576866a9ff792e16e0f91a658d2e
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-14 12:41:35 +0530

    Header layout / bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./containers/Home";
+import Signin from "./containers/Signin";
+import Signup from "./containers/Signup";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/signin" component={Signin}></Route>
+          <Route path="/signup" component={Signup}></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
