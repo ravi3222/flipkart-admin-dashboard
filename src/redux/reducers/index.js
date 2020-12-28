86e869e08f554e49505892856374e12088ca0729
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-28 09:16:13 +0530

    Category actions and reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth.reducers";
+import categoryReducer from "./category.reducers";
 import userReducer from "./user.reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   user: userReducer,
+  category: categoryReducer,
+  // product: productReducer,
+  // order: orderReducer,
 });
 
 export default rootReducer;
