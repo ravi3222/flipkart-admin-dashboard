6829926b856a53bce71bdbae31ceb8072fbe8832
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-28 09:19:26 +0530

    Category dashboard / Fetch and show all categories from backend

---
+++ src/containers/Category/index.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Layout from "../../components/Layout";
+import { getAllCategory } from "../../redux/actions";
+
+function Category() {
+  const category = useSelector((state) => state.category);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllCategory());
+  }, []);
+
+  const renderCategories = (categories) => {
+    let mycategories = [];
+
+    for (let category of categories) {
+      mycategories.push(
+        <li key={category.name}>
+          {category.name}
+          {category.children.length > 0 ? (
+            <ul>{renderCategories(category.children)}</ul>
+          ) : null}
+        </li>
+      );
+    }
+
+    return mycategories;
+  };
+  return (
+    <Layout sidebar>
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <h3>Category</h3>
+              <button>Add</button>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <ul>{renderCategories(category.categories)}</ul>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
+}
+
+export default Category;
