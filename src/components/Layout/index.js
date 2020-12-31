d33947495955b0bd4bdb8b9c8a49c1d8c6ef6a87
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-31 13:27:17 +0530

    Adding styles on sidebar layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import Header from "../Header";
+import "./styles.css";
 
 function Layout({ children, sidebar }) {
   return (
