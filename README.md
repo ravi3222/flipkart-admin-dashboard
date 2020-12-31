bfadd87b83c4cdb895d9a85118ee907228f80973
ravi3222 <kashyap.ravi3222@gmail.com>
2020-12-31 13:31:21 +0530

    Build new categories list on adding new single category

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# Flipkart Admin Dashboard
+<h1> Flipkart Admin Dashboard </h1>
 
-## Screenshots
+<h2>Screenshots</h2>
 
 ![category-dashboard](docs/images/category-dashboard.png)
+
+<h2>🚀 Some Tools I Use</h2>
+
+<p align="left">
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="100" height="100" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="100" height="100" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="100" height="100" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg" alt="bootstrap" width="100" height="100" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="100" height="100" />
+
+<img src="https://devicons.github.io/devicon/devicon.git/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="100" height="100" />
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="100" height="100" />
+</p>
--- src/redux/reducers/category.reducers.js
+++ src/redux/reducers/category.reducers.js
@@ -6,6 +6,44 @@ const initialState = {
   error: null,
 };
 
+const buildNewCategories = (parentId, categories, category) => {
+  let myCategories = [];
+  for (let cat of categories) {
+    if (cat._id === parentId) {
+      myCategories.push({
+        ...cat,
+        children:
+          cat.children && cat.children.length > 0
+            ? buildNewCategories(
+                parentId,
+                [
+                  ...cat.children,
+                  {
+                    _id: category._id,
+                    name: category.name,
+                    parentId: category.parentId,
+                    children: category.children,
+                    slug: category.slug,
+                  },
+                ],
+                category
+              )
+            : [],
+      });
+    } else {
+      myCategories.push({
+        ...cat,
+        children:
+          cat.children && cat.children.length > 0
+            ? buildNewCategories(parentId, cat.children, category)
+            : [],
+      });
+    }
+  }
+
+  return myCategories;
+};
+
 const categoryReducer = (state = initialState, action) => {
   switch (action.type) {
     case categoryConstants.GET_ALL_CATEGORIES_SUCCESS:
@@ -23,8 +61,16 @@ const categoryReducer = (state = initialState, action) => {
       break;
 
     case categoryConstants.ADD_NEW_CATEGORY_SUCCESS:
+      const category = action.payload.category;
+      const updatedCategories = buildNewCategories(
+        category.parentId,
+        state.categories,
+        category
+      );
+      console.log("updatedCategories", updatedCategories);
       state = {
         ...state,
+        categories: updatedCategories,
         loading: false,
       };
       break;
